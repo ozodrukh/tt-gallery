b252d87b20e8101b17685bb9e890d6faf74a190d
Ozodrukh <ozik.dev@gmail.com>
2024-04-02 18:14:58 +0500

    fix for sliders

--- yt_dlp_plugins/extractor/ttgalery_extractor.py
+++ yt_dlp_plugins/extractor/ttgalery_extractor.py
@@ -4,12 +4,14 @@ from yt_dlp.version import __version__
 if version_tuple(__version__) < (2023, 9, 24):
     raise ImportError('yt-dlp version 2023.09.24 or later is required to use the TTUser plugin')
 
-from yt_dlp.extractor.tiktok import TikTokIE
+from yt_dlp.extractor.tiktok import TikTokPhotoIE
 
-class TikTokGalleryIE(TikTokIE, plugin_name="TTGalery"):
+class TikTokGalleryIE(TikTokPhotoIE, plugin_name="TTGalery"):
+    _VALID_URL = r'https?://www\.tiktok\.com/(?:embed|@(?P<user_id>[\w\.-]+)?/(?:video|photo))/(?P<id>\d+)'
+    _EMBED_REGEX = [rf'<(?:script|iframe)[^>]+\bsrc=(["\'])(?P<url>{_VALID_URL})']
+
     def _parse_aweme_video_app(self, aweme_detail):
         extracted = super()._parse_aweme_video_app(aweme_detail)
-
         if 'image_post_info' in aweme_detail:
             tt_gallery_slides = aweme_detail['image_post_info']['images']
 
